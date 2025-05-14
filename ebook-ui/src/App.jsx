1b0ca214ec6fb502ef0627e5a016866a7fe656ef
waseem ahmad <waseemahmad9977@gmail.com>
2025-05-14 18:53:04 +0530

    ui work

---
+++ ebook-ui/src/App.jsx
@@ -0,0 +1,18 @@
+import 'animate.css';
+import 'remixicon/fonts/remixicon.css'
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Admin from "./components/Admin"
+ 
+const App = ()=>{
+  return (
+    <div>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      
+      <Admin/>
+    </div>
+  )
+}
+
+export default App
